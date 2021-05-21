1f794bf5cf8d6fa3e07be2c4816fe79f93da7212
kunalchitara <kunal.chitara@gmail.com>
2021-05-21 14:42:26 +0530

    Revert "BT Fix"
    This reverts commit 97de457d64352b45445c5d0c72a99e7e01cac89a.

--- msm8916.mk
+++ msm8916.mk
@@ -113,6 +113,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
     libbt-vendor \
     android.hardware.bluetooth@1.0 \
