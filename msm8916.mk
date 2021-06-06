3705d11897c69d3d9b9e82869c97bf2a46de77d3
nathanielwesley <82140646+nathanielwesley@users.noreply.github.com>
2021-06-06 14:19:24 +0530

    Update msm8916.mk

--- msm8916.mk
+++ msm8916.mk
@@ -462,9 +462,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Reduces GC frequency of foreground apps by 50%
 PRODUCT_PROPERTY_OVERRIDES += dalvik.vm.foreground-heap-growth-multiplier=2.0
 
-# Boot Animtion
-TARGET_BOOTANIMATION_HALF_RES := true
-
 # Smooth Streaming for Video Playback
 PRODUCT_PROPERTY_OVERRIDES += \
      mm.enable.sec.smoothstreaming=true
